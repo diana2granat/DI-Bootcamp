a6c4a640ca4ed431aaee36f77788f1256b6eca48
Diana <diana2granat@gmail.com>
2024-03-17 14:26:16 +0200

    Week3Day3 DailyCHallenge

--- Week3/Day3/Challenge/challenge.py
+++ Week3/Day3/Challenge/challenge.py
@@ -12,29 +12,70 @@
 #     Be able to compare two circles to see which is bigger, and return a Boolean - use a dunder method
 #     Be able to compare two circles and see if there are equal, and return a Boolean- use a dunder method
 #     Be able to put them in a list and sort them
-#     Bonus (not mandatory) : Install the Turtle module, and draw the sorted circles
-
+#======================================================answer=====================================================
 
 import math
 
 class Circle():
 
     def __init__(self, radius=None, diameter=None) -> None:
-        self.diameter = radius * 2
-        self.radius = radius
+        if radius is not None:
+            self.radius = radius
+            self.diameter = radius * 2
+        elif diameter is not None:
+            self.diameter = diameter
+            self.radius = diameter / 2
+        else:
+            raise ValueError("radius or diameter must be provided")
     
     def circle(self):
         print(self.diameter, self.radius)
 
     def circle_area(self):
-        area = math.pi * self.radius**2
-        print(area)
-        return area
+        return math.pi * self.radius**2
 
     def __str__(self):
         area = self.circle_area()
         return f"Radius: {self.radius}, Diameter: {self.diameter}, Area: {area}"
-
+    
+    # Be able to add two circles together, and return a new circle with the new radius - use a dunder method
+    def __add__(self, other):
+        if isinstance(other, Circle):
+            return Circle(radius=self.radius + other.radius)
+        else:
+            raise TypeError("Unsupported operand type for +: Circle and {}".format(type(other)))
+    
+    # Be able to compare two circles to see which is bigger, and return a Boolean - use a dunder method
+    def __lt__(self, other):
+        if isinstance(other, Circle):
+            return self.radius < other.radius
+        else:
+            raise TypeError("Unsupported operand type for <: Circle and {}".format(type(other)))
+    
+    # Be able to compare two circles and see if there are equal, and return a Boolean- use a dunder method
+    def __eq__(self, other):
+        if isinstance(other, Circle):
+            return self.radius == other.radius
+        else:
+            raise TypeError("Unsupported operand type for ==: Circle and {}".format(type(other)))
 
 circle1 = Circle(2)
 print(circle1)
+
+circle2 = Circle(3)
+circle3 = Circle(3)
+
+# Adding two circles together
+result_circle = circle1 + circle2
+print("Circle after addition:", result_circle)
+
+# Comparing two circles to see which is bigger
+print("Circle 1 is bigger than Circle 2:", circle1 > circle2)
+
+# Comparing two circles to see if they are equal
+print("Circle 2 is equal to Circle 3:", circle2 == circle3)
+
+# Putting circles in a list and sorting them
+circles = [circle1, circle2, circle3]
+circles.sort()
+print("Sorted circles by radius:", [str(circle) for circle in circles])
